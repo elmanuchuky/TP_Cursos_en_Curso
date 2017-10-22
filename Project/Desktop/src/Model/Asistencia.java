471b94be1990d350b7971ccb3251909def28c68a
MagaPache <32725932+MagaPache@users.noreply.github.com>
2017-10-21 22:30:58 -0300

    He creado la clase asistencia, joder tio....

--- Project/Desktop/src/Model/Asistencia.java
+++ Project/Desktop/src/Model/Asistencia.java
@@ -2,6 +2,45 @@ package Model;
 
 public class Asistencia {
     
-    
-    
+    private int inscripcion;
+    private String fechaAsistencia;
+    private boolean estaPresente;
+
+    public int getInscripcion() {
+        return inscripcion;
+    }
+
+    public void setInscripcion(int inscripcion) {
+        this.inscripcion = inscripcion;
+    }
+
+    public String getFechaAsistencia() {
+        return fechaAsistencia;
+    }
+
+    public void setFechaAsistencia(String fechaAsistencia) {
+        this.fechaAsistencia = fechaAsistencia;
+    }
+
+    public boolean isEstaPresente() {
+        return estaPresente;
+    }
+
+    public void setEstaPresente(boolean estaPresente) {
+        this.estaPresente = estaPresente;
+    }
+
+    public Asistencia() {
+    }
+
+    public Asistencia(int inscripcion, String fechaAsistencia, boolean estaPresente) {
+        this.inscripcion = inscripcion;
+        this.fechaAsistencia = fechaAsistencia;
+        this.estaPresente = estaPresente;
+    }
+
+    @Override
+    public String toString() {
+        return "Asistencia{" + "inscripcion=" + inscripcion + ", fechaAsistencia=" + fechaAsistencia + ", estaPresente=" + estaPresente + '}';
+    }   
 }
