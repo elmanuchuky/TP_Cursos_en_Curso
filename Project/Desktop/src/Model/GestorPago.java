8608acb0b78ba6b42a36e4eb342b76cf3ebeb620
GabrielSoto95 <32778898+GabrielSoto95@users.noreply.github.com>
2017-10-26 15:09:38 -0300

    realizado
    reporte de inscriptos con sus documentos,
    pagos realizados buscando por mai
    adeudado buscado por mail
    pagos de una persona mostrando

--- Project/Desktop/src/Model/GestorPago.java
+++ Project/Desktop/src/Model/GestorPago.java
@@ -19,22 +19,23 @@ import java.util.ArrayList;
  * @author Gabriel
  */
 public class GestorPago {
+
     String conexion = "jdbc:sqlserver://localhost:1412;databaseName=Colegio_Informatica_Metodologia";
     String user = "Gabriel";
     String pass = "1234";
 
-    public void agregarPago(Pago p) throws ClassNotFoundException, SQLException{
+    public void agregarPago(Pago p) throws ClassNotFoundException, SQLException {
         forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
         Connection con = DriverManager.getConnection(conexion, user, pass);
         PreparedStatement comando = con.prepareStatement("exec sp_insert_pago ?,?");
-        comando.setInt(1,p.getInscripcion());
+        comando.setInt(1, p.getInscripcion());
         comando.setDouble(2, p.getMonto());
-        
+
         comando.executeUpdate();
         comando.close();
         con.close();
     }
-    
+
     public ArrayList<Pago> obtenerTodos() throws ClassNotFoundException, SQLException {
         forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
         ArrayList<Pago> pagos = new ArrayList<Pago>();
@@ -72,5 +73,66 @@ public class GestorPago {
         comando.close();
         con.close();
         return p;
+    }
+
+    //poder ver los pagos realizados filtrando por el mail
+    public ArrayList<VMPagosXMail> VerPagosPorMail(String mail) throws ClassNotFoundException, SQLException {
+        ArrayList<VMPagosXMail> lista = new ArrayList<>();
+        forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        Connection con = DriverManager.getConnection(conexion, user, pass);
+        Statement stmtIns = con.createStatement(); // idCurso, idCursante
+        ResultSet query = stmtIns.executeQuery("exec sp_ver_pagos_x_mail " + mail);
+
+        while (query.next()) {
+            VMPagosXMail datos = new VMPagosXMail();
+
+            datos.setNombre(query.getString("Curso"));
+            datos.setMonto(query.getFloat("Pago"));
+            datos.setFecha(query.getString("Fecha"));
+
+            lista.add(datos);
+        }
+        query.close();
+        stmtIns.close();
+        con.close();
+        return lista;
+    }
+
+    //muestra lo adeudado filtrando por mail
+    public VMPagosXMail mostrarDeuda(String mail) throws ClassNotFoundException, SQLException {
+        forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        Connection con = DriverManager.getConnection(conexion, user, pass);
+        Statement stmtIns = con.createStatement(); // idCurso, idCursante
+        ResultSet query = stmtIns.executeQuery("sp_ver_adeudado_x_mail " + mail);
+
+        VMPagosXMail datos = new VMPagosXMail();
+        if (query.next()) {
+
+            datos.setNombre(query.getString("Curso"));
+            datos.setMonto(query.getFloat("Pago"));
+        }
+        query.close();
+        stmtIns.close();
+        con.close();
+        return datos;
+    }
+// mostrar pagos de una persona con fecha y el monto
+    public ArrayList<VMPagosXMail> PagosPorFecha(int i) throws ClassNotFoundException, SQLException {
+        ArrayList<VMPagosXMail> lista = new ArrayList<>();
+        forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        Connection con = DriverManager.getConnection(conexion, user, pass);
+        Statement stmtIns = con.createStatement(); // idCurso, idCursante
+        ResultSet query = stmtIns.executeQuery("sp_consultar_pagos_totales_x_inscripto " + i);
+
+        if (query.next()) {
+            VMPagosXMail datos = new VMPagosXMail();
+            
+            datos.setFecha(query.getString("Fecha"));
+            datos.setMonto(query.getFloat("Pago"));
+        }
+        query.close();
+        stmtIns.close();
+        con.close();
+        return lista;
     }
 }
